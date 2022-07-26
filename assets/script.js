7ebd3ccfd54947b1c45fa2ee12ec93fd38f60d53
Dante Rodriguez <rodriguezdante89@yahoo.com>
2022-07-26 18:44:09 -0400

    minor html and css updates

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+// connect image api to show image for questions //
+var questions = [
+    {
+        question:
+        choices: []
+        answer:
+    }]
+
+// connect description api to show description for questions //
+
+
+// add functionality to buttons //
