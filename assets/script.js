414d33d32220734d165f83c8aaf3d9567778c02f
chuckd23 <107444134+chuckd23@users.noreply.github.com>
2022-07-26 20:14:19 -0400

    update buttons on html

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,2 @@
-// connect image api to show image for questions //
-var questions = [
-    {
-        question:
-        choices: []
-        answer:
-    }]
-
-// connect description api to show description for questions //
-
-
-// add functionality to buttons //
+//set variable to select image//
+//
