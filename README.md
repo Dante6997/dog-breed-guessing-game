0896610cfc46fcc81e87dc228247a54665dd5390
Dante Rodriguez <rodriguezdante89@yahoo.com>
2022-07-27 23:51:16 -0400

    incomplete README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# group-project
+# Dog-Breed-Guessing-Game
+### Deployed Link
+## Project 1
+- ### My group and I were tasked to develop our own website using at least 2 server side  API's while using a CSS framework other than Bootstrap.
+- ### Our website displayed an image from the dog.ceo API and gave 5 answer choices. One of the answer choices is a dog breed that relates the the image. We also used another API called DogDummyAPI to get a description of the dog breed to pop up when a button is clicked.
+- ### THe technologies we used were API's, JavaScript, HTML, CSS, Skeleton, and Bootstrap. 
--- assets/script.js
+++ assets/script.js
@@ -7,14 +7,36 @@ var btn4El = document.getElementById("btn4")
 var btn5El = document.getElementById("btn5")
 var btnDivEl = document.getElementById("btns")
 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '48b7163cdamshe822b76bf59eb35p1b1960jsnb634b45f19f6',
+		'X-RapidAPI-Host': 'dogdummyapi.p.rapidapi.com'
+	}
+};
 
 btnDivEl.addEventListener("click", game);
 
-function game() {
-    var dogFacts = "https://dogdummyapi.p.rapidapi.com/dogs/filter?minLifeExpectancy=12"
+function game(event) {
+    var dogBreed = event.target.textContent;
+    var dogFacts = "https://dogdummyapi.p.rapidapi.com/dogs/filter?search=" + dogBreed;
 
-    fetch(dogFacts).then(function.response {
-       
-    }
+    fetch(dogFacts, options)
+        .then(function(response){return response.json()})
+        .then(function(dogs){
+            var dog = dogs[0];
+            descriptionEl.innerHTML = dog.description;
+
+        })
 
 }
+
+function imgSelector() {
+    
+}
+
+
+"https://images.dog.ceo/breeds/chihuahua/n02085620_3045.jpg"
+"https://images.dog.ceo/breeds/sheepdog-shetland/n02105855_19002.jpg"
+"https://images.dog.ceo/breeds/collie-border/n02106166_2198.jpg"
+
