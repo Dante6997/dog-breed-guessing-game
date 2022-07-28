35bd378ae491b100a66e08271418b487e7578032
Dante Rodriguez <rodriguezdante89@yahoo.com>
2022-07-27 21:42:41 -0400

    dog names added

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,20 @@
-//set variable to select image//
-//
+var imgEl = document.getElementById("pic")
+var descriptionEl = document.getElementById("description")
+var btn1El = document.getElementById("btn1")
+var btn2El = document.getElementById("btn2")
+var btn3El = document.getElementById("btn3")
+var btn4El = document.getElementById("btn4")
+var btn5El = document.getElementById("btn5")
+var btnDivEl = document.getElementById("btns")
+
+
+btnDivEl.addEventListener("click", game);
+
+function game() {
+    var dogFacts = "https://dogdummyapi.p.rapidapi.com/dogs/filter?minLifeExpectancy=12"
+
+    fetch(dogFacts).then(function.response {
+       
+    }
+
+}
